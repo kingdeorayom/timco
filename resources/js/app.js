9eef45e46f28f64d667000d3079fc8c0df85723a
serking <kingdeorayom@gmail.com>
2025-02-20 21:01:38 +0800

    Refactor tab navigation: remove commented code for cleaner implementation and improve readability

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,6 @@ $(document).ready(function () {
 
 $(document).ready(function () {
     $("ul li").click(function () {
-        // Remove active class from all tabs
         $("ul li")
             .removeClass(
                 "text-timco-dark-blue border-timco-dark-blue font-bold"
@@ -17,24 +16,19 @@ $(document).ready(function () {
                 "border-transparent hover:text-timco-dark-blue hover:border-timco-dark-blue"
             );
 
-        // Add active class to the clicked tab
         $(this)
             .removeClass(
                 "border-transparent hover:text-timco-dark-blue hover:border-timco-dark-blue"
             )
             .addClass("text-timco-dark-blue border-timco-dark-blue font-bold");
 
-        // Get the corresponding content ID from data-target
         let contentId = $(this).data("target");
 
-        // Hide all content divs
         $(".machine-content").hide();
 
-        // Show the corresponding content div
         $("#" + contentId).show();
     });
 
-    // Hide all content divs except the first one on page load
     $(".machine-content").hide();
     $(".machine-content:first").show();
 });
